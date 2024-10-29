760e6ba2ef39bb927dbabe2b80904afc4d5b14cc
sandy0v0 <be0324ss@gmail.com>
2024-10-29 10:29:19 +0800

    Comments 註解

---
+++ s20241029/s20241029_04_comments.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>註解</title>
+</head>
+<body>
+
+html
+<!-- html -->
+<!-- ctrl + / -->
+
+<?php
+
+// data ="hello" 
+
+?>
+
+XAMPP Config
+
+<!-- 
+註解用 # 
+例如:
+#DocumentRoot "E:/web"
+#<Directory "E:/web"> 
+-->
+
+
+
+</body>
+</html>
