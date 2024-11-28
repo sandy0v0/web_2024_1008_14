c79108d0f846276c5a651d12cbabff57114939a5
sandy0v0 <be0324ss@gmail.com>
2024-11-28 10:02:28 +0800

    js basic 介紹

---
+++ s20241128/test.php
@@ -0,0 +1,20 @@
+<?php
+    $name = 'jia-3';
+    echo("Hi $name");
+
+    //  array
+
+    // index array
+    $indexArr =[1,2,3];
+
+    // Associative array
+    $assocArr = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
+
+
+
+
+?>
