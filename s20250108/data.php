c761c27f9dae19ec72e842f426d7551411fb7ae0
sandy0v0 <be0324ss@gmail.com>
2025-01-08 10:15:36 +0800

    3間分公司的資料, 用Array的方式呈現資料, 接著用BS的table/data/foreach的方式呈現

---
+++ s20250108/data.php
@@ -0,0 +1,33 @@
+<?php
+
+// 3間分公司的資料, 用Array的方式呈現資料, 在使用BS的table/data/foreach的方式呈現
+
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$data = [
+    [
+        'id' => 1,
+        'name' => '台北店',
+        'mobile' => '0911-111-111',
+        'address' => '台北市中正路1號'
+    ],
+    [
+        'id' => 2,
+        'name' => '台中店',
+        'mobile' => '0922-222-222',
+        'address' => '台中市中正路2號'
+    ],
+    [
+        'id' => 3,
+        'name' => '高雄店',
+        'mobile' => '0933-333-333',
+        'address' => '高雄市中正路3號'
+    ]
+];
+
+dd($data);
