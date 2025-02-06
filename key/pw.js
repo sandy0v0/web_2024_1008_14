1ad346bd38403ef23501ab116dd9532f67ca8335
sandy0v0 <be0324ss@gmail.com>
2025-02-06 09:07:49 +0800

    天氣API 的icon

---
+++ key/pw.js
@@ -0,0 +1,5 @@
+// 天氣 API 的授權碼
+let pw = 'CWA-C5A0D012-D54B-42FD-A238-F7F3DAEBC74A';
+
+// Request URL
+// https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-C5A0D012-D54B-42FD-A238-F7F3DAEBC74A
